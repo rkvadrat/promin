821211e526c52ebd89405335ebaef22bd38af7f5
Roman <romakvadrat@icloud.com>
2020-05-26 16:10:02 +0300

    promin

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 var slideEl = $(".slide--parent");
 
 slideEl.flickity({
-	imagesLoaded: true,
-	wrapAround: true,
-	autoPlay: true,
-	pauseAutoPlayOnHover: false
+  imagesLoaded: true,
+  wrapAround: true,
+  autoPlay: true,
+  pauseAutoPlayOnHover: false,
 });
 
-
-// 
+//
 new WOW().init();
 
 //
 
-$('.sl').slick({
+// $('.sl').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+// });
+
+$(".sl").slick({
+  dots: true,
   infinite: true,
+  speed: 300,
   slidesToShow: 3,
-  slidesToScroll: 3,
+  centerMode: true,
+  variableWidth: true,
 });
-	
